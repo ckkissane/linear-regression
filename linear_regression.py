823ef216a2388754aae56a0b981d901079dd777d
ckkissane <67170576+ckkissane@users.noreply.github.com>
2022-04-15 10:29:56 -0400

    remove batched gradient descent for simplicity

---
+++ linear_regression.py
@@ -0,0 +1,30 @@
+import random
+
+from stochastic_gradient_descent import minimize_stochastic
+
+def predict(w, b, x_i):
+    return w * x_i + b
+
+def error(w, b, x_i, y_i):
+        return y_i - predict(w, b, x_i)
+
+def squared_error(x_i, y_i, theta):
+    w, b = theta
+    return error(w, b, x_i, y_i) ** 2
+
+def squared_error_gradient(x_i, y_i, theta):
+    w, b = theta
+    return [-2 * x_i * error(w, b, x_i, y_i), -2 * error(w, b, x_i, y_i)]
+
+if __name__ == '__main__':
+    x_data = [1, 2, 3]
+    y_data = [3, 5, 7]
+
+    random.seed(0)
+    theta = [random.random(), random.random()]
+
+    w_star, b_star = minimize_stochastic(squared_error, squared_error_gradient, x_data, y_data, theta, 0.0001)
+    x_new = 4
+    y_new = predict(w_star, b_star, x_new)
+    print("prediction for x =", x_new, ", y =", y_new)
+
--- stochastic_gradient_descent.py
+++ stochastic_gradient_descent.py
@@ -1,19 +1,5 @@
 import random
 
-def predict(w, b, x_i):
-    return w * x_i + b
-
-def error(w, b, x_i, y_i):
-        return y_i - predict(w, b, x_i)
-
-def squared_error(x_i, y_i, theta):
-    w, b = theta
-    return error(w, b, x_i, y_i) ** 2
-
-def squared_error_gradient(x_i, y_i, theta):
-    w, b = theta
-    return [-2 * x_i * error(w, b, x_i, y_i), -2 * error(w, b, x_i, y_i)]
-
 def vector_subtract(v, w):
     return [v_i - w_i for v_i, w_i in zip(v, w)]
 
@@ -48,15 +34,3 @@ def minimize_stochastic(target_fn, gradient_fn, x, y, theta_0, alpha_0=0.01):
             theta = vector_subtract(theta, scalar_multiply(alpha, gradient_i))
 
     return min_theta
-
-if __name__ == '__main__':
-    x_data = [1, 2, 3]
-    y_data = [3, 5, 7]
-
-    random.seed(0)
-    theta = [random.random(), random.random()]
-
-    w_star, b_star = minimize_stochastic(squared_error, squared_error_gradient, x_data, y_data, theta, 0.0001)
-    x_new = 4
-    y_new = predict(w_star, b_star, x_new)
-    print("prediction for x =", x_new, ", y =", y_new)
